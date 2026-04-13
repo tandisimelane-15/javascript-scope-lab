7f5ec8aac498576395d8aea448c6256f53e572d5
Abigail Tandiwe Simelane <tandisimelane24@gmail.com>
2026-04-13 15:02:57 +0300

    final changes to javascript scope lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+
+//global variables
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+//function scoped variables
+function addBurger(){
+    const newBurger = "Flatburger";
+    burgers.push(newBurger);
+}
+
+//block-scoped variables
+if(true){
+    const anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger);
+}
+
+//changing featured drink
+function changeFeaturedDrink(){
+    featuredDrink = "The JavaShake";
+}
